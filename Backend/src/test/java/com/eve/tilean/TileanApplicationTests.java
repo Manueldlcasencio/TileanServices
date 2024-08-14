acfafa227294f7f27ef20200de8a1d3abd64ed95
Manueldlcasencio <manuel.dlcasencio@gmail.com>
2024-08-14 20:51:06 +0200

    Initial commit

---
+++ Backend/src/test/java/com/eve/tilean/TileanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eve.tilean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TileanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
